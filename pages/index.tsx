841d2b1e4c287b6e14f850f08c8d0c1922e1ccc9
zeyadelshaarawy <shaarawyziad121@gmail.com>
2023-04-20 22:53:12 +0200

    INITALLIZATION

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { Inter } from "next/font/google";
+
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return <><h1>Hello world</h1></>;
+}
