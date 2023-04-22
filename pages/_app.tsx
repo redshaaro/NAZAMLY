e0bf28e9dc8332100533b8e452d3059d482770cc
zeyadelshaarawy <shaarawyziad121@gmail.com>
2023-04-22 19:42:59 +0200

    test pull2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,15 @@
 import "@/styles/globals.css";
-import { ClerkProvider, SignIn, SignUp, SignedIn, SignedOut } from "@clerk/nextjs";
+
 
 export default function App({ Component, pageProps }) {
   const clerkpub=process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
   console.log(clerkpub)
   return (
-    <ClerkProvider>
-      <SignedIn>
+     
+    
         <Component {...pageProps} />
-      </SignedIn>
-      <SignedOut>
-        <SignIn></SignIn>
-      </SignedOut>
-    </ClerkProvider>
+       
+      
+    
   );
 }
