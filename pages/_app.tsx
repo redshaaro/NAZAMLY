e6dbbbe5515cae09cd14919ef2559173aa54519a
aslw1 <Abdelrahman.Eldahab@student.aast.edu>
2023-04-22 19:44:28 +0200

    test-push

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import "@/styles/globals.css";
 
 
 export default function App({ Component, pageProps }) {
-  const clerkpub=process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
-  console.log(clerkpub)
   return (
      
     
