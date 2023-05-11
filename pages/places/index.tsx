e39ccc38996215fd41e0b1856ad4b7bffd14027a
zeyadelshaarawy <shaarawyziad121@gmail.com>
2023-05-11 03:54:29 +0300

    fixed

--- pages/places/index.tsx
+++ pages/places/index.tsx
@@ -1,20 +1,10 @@
 import VenueCard from "@/Components/VenueCard";
-import { Card, CardMedia, CardContent, Typography } from "@mui/material";
-
-import useSWR from "swr";
-import Link from "next/link";
 import { useState } from "react";
 import NavBar from "@/Components/NavBar";
-import { filter } from "@/lib/filter";
-
-const Index = () => {
-  const url = process.env.NEXT_PUBLIC_API_URL;
-  const { data: venues, error } = useSWR(`${url}/venues`);
-
- 
+import { CardProps } from "@/utils/interfaces";
 
 export async function getStaticProps() {
-  const res = await fetch("https://nazamly.vercel.app/api/venues", {
+  const res = await fetch("http://localhost:3000/api/venues", {
     method: "GET",
   });
   const venues = await res.json();
@@ -24,64 +14,79 @@ export async function getStaticProps() {
     },
   };
 }
-const Index = ({ venues }) => {
+const Index = ({ venues }: CardProps) => {
   const [price, setPrice] = useState("");
   const [location, setLocation] = useState("");
-  const [filtered, setFiltered] = useState([]);
-
-  let venuesToRender = filtered.length ? filtered : venues;
-
+  const [filtered, setFiltered] = useState<CardProps>();
+  const filter = async (price, location) => {
+    const res = await fetch(
+      `http://localhost:3000/api/venues/filter/?location=${location}&price=${price}$`,
+      {
+        method: "GET",
+      }
+    );
+    const filtered = await res.json();
+    setFiltered(filtered);
+  };
+  let venuesToRender = filtered?.length ? filtered : venues;
   return (
     <div>
       <NavBar color={"black"}></NavBar>
-      <select
-        className="mt-4 p-4 text-2xl focus:outline-none cursor-pointer"
-        value={location}
-        onChange={(event) => {
-          setLocation(event.target.value);
-          filter(price, event.target.value, setFiltered);
-        }}
-      >
-        <option value="">All</option>
-        <option value="Alexandria">Alexandria</option>
-        <option value="Cairo">Cairo</option>
-        <option value="Luxor">Luxor</option>
-      </select>
-      <select
-        className="mt-4 p-4 text-2xl focus:outline-none cursor-pointer"
-        value={price}
-        onChange={(event) => {
-          setPrice(event.target.value);
+      <ul className="flex gap-10 pl-7 mt-10">
+        <li>
+          <label
+            htmlFor="location"
+            className="block mb-2 text-base font-medium text-gray-900 dark:text-white"
+          >
+            Select a location
+          </label>
+          <select
+            id="location"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-900 focus:border-blue-900 block w-[130px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-900 dark:focus:border-blue-900"
+            value={location}
+            onChange={(event) => {
+              setLocation(event.target.value);
+              filter(price, event.target.value);
+            }}
+          >
+            <option value="">All</option>
+            <option value="Alexandria">Alexandria</option>
+            <option value="Cairo">Cairo</option>
+            <option value="Luxor">Luxor</option>
+          </select>
+        </li>
+        <li>
+          <label
+            htmlFor="price"
+            className="block mb-2 text-base font-medium text-gray-900 dark:text-white"
+          >
+            Select a price
+          </label>
+          <select
+            id="price"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[130px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value={price}
+            onChange={(event) => {
+              setPrice(event.target.value);
 
-          filter(event.target.value, location, setFiltered);
-        }}
-      >
-        <option value="">All</option>
-        <option value="200">200$</option>
-        <option value="85">85$</option>
-        <option value="100">100$</option>
-      </select>
+              filter(event.target.value, location);
+            }}
+          >
+            <option value="">All</option>
+            <option value="200">200$</option>
+            <option value="85">85$</option>
+            <option value="100">100$</option>
+          </select>
+        </li>
+      </ul>
       <div className="flex justify-between items-center flex-wrap p-6 mt-3">
-        {venuesToRender?.map((venue) => (
-          <Card sx={{ width: 200, height: 300 }} key={venue.id}>
-            <CardMedia
-              sx={{ height: 140 }}
-              image={venue.Image}
-              title={venue.Name}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {venue.Name}
-              </Typography>
-              <Typography gutterBottom variant="h5" component="div">
-                {venue.Available ? "Available" : "Reserved"}
-              </Typography>
-              <Link href={`/places/${venue.id}`}>
-                <p>Explore it</p>
-              </Link>
-            </CardContent>
-          </Card>
-        ))}
+        <ul className="flex flex-col md:flex-row gap-5 justify-center">
+          {venuesToRender.map((venue: CardProps["venue"]) => (
+            <li key={venue.id}>
+              <VenueCard venue={venue}></VenueCard>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
