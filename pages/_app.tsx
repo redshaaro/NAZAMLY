e08f819054e025085e081cafd2ff83887ab68f45
zeyadelshaarawy <shaarawyziad121@gmail.com>
2023-05-12 14:55:02 +0300

    fixed5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import AppContextProvider from "../context/appContext";
+import NavBar from "@/Components/NavBar";
  
 
 export default function App({ Component, pageProps }) {
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <AppContextProvider>
+        <NavBar color={undefined}></NavBar>
         
           <Component {...pageProps} />
        
