662615e0ad32e18343c11441cfdcbcade73ef809
zeyadelshaarawy <shaarawyziad121@gmail.com>
2023-05-12 22:19:13 +0300

    fixed

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import Navbar from "./NavBar";
  
-import img from "../assets/distribution-header-footer.jpg";
-// import Image from 'next/image'
+import img from "../public/distribution-header-footer.jpg";
+import Image from 'next/image'
 // import { useInView } from "react-intersection-observer";
 const Hero = () => {
   // const { ref: firstRef, inView: first } = useInView();
   return (
     <section id="hero-1" className=" h-[650px] relative">
       <Navbar color="black"></Navbar>
-      <img
+      <div className=" brightness-90 z-[-1] h-[700px] w-screen top-0 absolute">
+      <Image
         src={img}
         className=" brightness-90 z-[-1] h-[700px] w-screen top-0 absolute"
         width={1200}
         height={800}
         alt="nth"
         />
+        </div>
           
 
       <h1 className="text-white   text-5xl font-bold text-center mt-20 capitalize ">
